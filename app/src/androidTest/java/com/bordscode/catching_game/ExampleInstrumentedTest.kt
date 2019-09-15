0ca99e36775d33f561990874a216b4b6a3544488
MichAca <m.aca@outlook.ph>
2019-09-16 06:14:59 +0800

    initial commit

---
+++ app/src/androidTest/java/com/bordscode/catching_game/ExampleInstrumentedTest.kt
@@ -0,0 +1,24 @@
+package com.bordscode.catching_game
+
+import android.support.test.InstrumentationRegistry
+import android.support.test.runner.AndroidJUnit4
+
+import org.junit.Test
+import org.junit.runner.RunWith
+
+import org.junit.Assert.*
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+@RunWith(AndroidJUnit4::class)
+class ExampleInstrumentedTest {
+    @Test
+    fun useAppContext() {
+        // Context of the app under test.
+        val appContext = InstrumentationRegistry.getTargetContext()
+        assertEquals("com.bordscode.catching_game", appContext.packageName)
+    }
+}
