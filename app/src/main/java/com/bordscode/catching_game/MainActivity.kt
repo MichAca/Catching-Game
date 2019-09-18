dffc1fbe699087d272db583d0c532c18beb60d65
MichAca <m.aca@outlook.ph>
2019-09-18 11:57:36 +0800

    fix layout

--- app/src/main/java/com/bordscode/catching_game/MainActivity.kt
+++ app/src/main/java/com/bordscode/catching_game/MainActivity.kt
@@ -2,11 +2,20 @@ package com.bordscode.catching_game
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        imageView1.setOnClickListener {
+
+
+        }
+
+
     }
 }
